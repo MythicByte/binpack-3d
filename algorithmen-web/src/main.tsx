453183406f953364dc18f1204730640d97e66c23
Markus Götz <57562344+MythicByte@users.noreply.github.com>
2026-04-04 06:05:20 +0200

    web updated

--- algorithmen-web/src/main.tsx
+++ algorithmen-web/src/main.tsx
@@ -7,5 +7,6 @@ import szene from "./szene.tsx";
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <App />
+    <szene />
   </StrictMode>,
 );
--- algorithmen-web/src/szene.tsx
+++ algorithmen-web/src/szene.tsx
@@ -1,4 +1,16 @@
 import * as three from "jsr:@3d/three";
 export function szene() {
-  const camera = new three.Per;
+  return (<>
+  const scene = three.Scene;
+  const camera = new three.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1 ,1000);
+  const render = new three.WebGLRenderer();
+  render.setSize(window.innerWidth, window.innerHeight);
+  document.body.appendChild(render.domElement);
+
+
+  const geometry = new three.BoxGeometry(1,1,1);
+  const material =  new three.MeshBasicMaterial({color:0x00ff00});
+  const cube = new three.Mesh(geometry,material);
+  render.render(scene,camera);
+  </>);
 }
