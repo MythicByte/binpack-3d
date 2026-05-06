d90522dcf6b9504dfb36a1d0256d001a9d996114
Markus Götz <57562344+MythicByte@users.noreply.github.com>
2026-05-07 01:45:06 +0200

    Remove web test code

--- algorithmen-web/vite.config.ts
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-import tailwindcss from "npm:@tailwindcss/vite";
-import wasm from "npm:vite-plugin-wasm";
-
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [react(), tailwindcss(), wasm()],
-});
